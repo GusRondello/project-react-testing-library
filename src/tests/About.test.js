44c2c9f479ad910d0a34f33bb0a49d03662bb528
Gustavo Rondello <gustavorondello99@outlook.com>
2022-03-22 17:46:56 -0300

    Requisito 3 completo

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -16,14 +16,18 @@ describe('Teste o componente <About.js />.', () => {
 
   it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
     renderWithRouter(<About />);
+
     const aboutHeading = screen.getByRole('heading', { name: /about pokédex/i });
+
     expect(aboutHeading).toBeInTheDocument();
   });
 
   it('Teste se a página contém a imagem correta de uma Pokédex', () => {
     renderWithRouter(<About />);
+
     const imgUrl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const imgRole = screen.getByRole('img', { src: imgUrl });
+
     expect(imgRole).toHaveAttribute('src', imgUrl);
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é a mensagem correta na tela se a pessoa não tiver pokemons favoritos',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+
+      const noFavPkm = screen.getByText(/no favorite pokemon found/i);
+      expect(noFavPkm).toBeInTheDocument();
+    });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    const detailsBtn = screen.getByRole('link', { name: /more details/i });
+    const homeBtn = screen.getByRole('link', { name: /home/i });
+    const nextPkmBtn = screen.getByTestId(/next-pokemon/i);
+    const favLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    // const favPkm = screen.getAllByTestId(/pokemon-name/i);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    userEvent.click(detailsBtn);
+    //  expect(pathname).toStrictEqual('/pokemon:25');
+    const favCheckbox = screen.getByLabelText('Pokémon favoritado?');
+
+    userEvent.click(favCheckbox);
+    userEvent.click(homeBtn);
+    expect(pathname).toBe('/');
+
+    userEvent.click(nextPkmBtn);
+    userEvent.click(detailsBtn);
+    // expect(pathname).toBe('pokemon:4');
+
+    userEvent.click(favCheckbox);
+    userEvent.click(favLink);
+    // expect(pathname).toBe('/favorites');
+
+    // expect(favPkm).toBeInTheDocument();
+    // expect(favPkm).toHaveTextContent(/pikachu/i);
+    // expect(favPkm).toHaveTextContent(/charmander/i);
+  });
+});
