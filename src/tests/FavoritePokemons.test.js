70e8ce353ad4ab54c73bde54d2fe4de71cd964d4
Gustavo Rondello <gustavorondello99@outlook.com>
2022-03-22 18:27:49 -0300

    Requisito 3 alterações para diminuir o teste e aprimorar o mesmo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,46 +1,25 @@
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import React from 'react';
-import App from '../App';
 import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
 import renderWithRouter from './renderWithRouter';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it('Teste se é a mensagem correta na tela se a pessoa não tiver pokemons favoritos',
     () => {
-      renderWithRouter(<FavoritePokemons />);
+      const pkmnArray = [];
+      renderWithRouter(<FavoritePokemons pokemons={ pkmnArray } />);
 
       const noFavPkm = screen.getByText(/no favorite pokemon found/i);
       expect(noFavPkm).toBeInTheDocument();
     });
   it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
-    const { history } = renderWithRouter(<App />);
-    const detailsBtn = screen.getByRole('link', { name: /more details/i });
-    const homeBtn = screen.getByRole('link', { name: /home/i });
-    const nextPkmBtn = screen.getByTestId(/next-pokemon/i);
-    const favLink = screen.getByRole('link', { name: /favorite pokémons/i });
-    // const favPkm = screen.getAllByTestId(/pokemon-name/i);
-
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
-    userEvent.click(detailsBtn);
-    //  expect(pathname).toStrictEqual('/pokemon:25');
-    const favCheckbox = screen.getByLabelText('Pokémon favoritado?');
-
-    userEvent.click(favCheckbox);
-    userEvent.click(homeBtn);
-    expect(pathname).toBe('/');
-
-    userEvent.click(nextPkmBtn);
-    userEvent.click(detailsBtn);
-    // expect(pathname).toBe('pokemon:4');
-
-    userEvent.click(favCheckbox);
-    userEvent.click(favLink);
-    // expect(pathname).toBe('/favorites');
-
-    // expect(favPkm).toBeInTheDocument();
-    // expect(favPkm).toHaveTextContent(/pikachu/i);
-    // expect(favPkm).toHaveTextContent(/charmander/i);
+    const pikachu = pokemons.find((pokemon) => pokemon.name === 'Pikachu');
+    const charmander = pokemons.find((pokemon) => pokemon.name === 'Charmander');
+    const pokeArray = [pikachu, charmander];
+    renderWithRouter(<FavoritePokemons pokemons={ pokeArray } />);
+    const favPkm = screen.getAllByTestId(/pokemon-name/i);
+    expect(favPkm[0]).toHaveTextContent(/pikachu/i);
+    expect(favPkm[1]).toHaveTextContent(/charmander/i);
   });
 });
