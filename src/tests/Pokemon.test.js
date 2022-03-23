72b788fa04c5d84b87ebc79c311be09474c90666
Gustavo Rondello <gustavorondello99@outlook.com>
2022-03-23 18:49:45 -0300

    Requisito 7 completo

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -7,13 +7,12 @@ import pokemons from '../data';
 import renderWithRouter from './renderWithRouter';
 
 describe('Teste o componente <Pokemon.js />', () => {
+  const pikachu = pokemons.find((pokemon) => pokemon.name === 'Pikachu');
+  const { name, type, averageWeight, image, id } = pikachu;
+  const { value, measurementUnit } = averageWeight;
+  renderWithRouter(<App />);
+
   it('É renderizado um card com as informações de determinado pokémon.', () => {
-    renderWithRouter(<App />);
-
-    const pikachu = pokemons.find((pokemon) => pokemon.name === 'Pikachu');
-    const { name, type, averageWeight, image } = pikachu;
-    const { value, measurementUnit } = averageWeight;
-
     const pkmName = screen.getByTestId('pokemon-name');
     const pkmType = screen.getByTestId('pokemon-type');
     const pkmWeight = screen.getByTestId('pokemon-weight');
@@ -27,21 +26,17 @@ describe('Teste o componente <Pokemon.js />', () => {
 
   it('Contém um link de navegação de detalhes do pokemon funcional', () => {
     const { history } = renderWithRouter(<App />);
-    const pikachu = pokemons.find((pokemon) => pokemon.name === 'Pikachu');
-    const { id } = pikachu;
-
     const detailsBtn = screen.getByRole('link', { name: /more details/i });
 
     userEvent.click(detailsBtn);
+
     const { pathname } = history.location;
     expect(pathname).toBe(`/pokemons/${id}`);
   });
 
   it('O Pokémon favoritado contém o esperado', () => {
-    const pikachu = pokemons.find((pokemon) => pokemon.name === 'Pikachu');
     renderWithRouter(<Pokemon pokemon={ pikachu } isFavorite />);
 
-    const { name } = pikachu;
     const favImg = screen.getByAltText(`${name} is marked as favorite`);
 
     expect(favImg).toHaveAttribute('src', '/star-icon.svg');
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import pokemons from '../data';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <PokemonDetails.js />', () => {
+  const pikachu = pokemons.find((pokemon) => pokemon.name === 'Pikachu');
+  const { name, foundAt, id } = pikachu;
+
+  it('As informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${id}`);
+
+    const detailsBtn = screen.queryByRole('link', { name: /more details/i });
+    const pokemonDetails = screen.getByRole('heading', { name: `${name} Details` });
+    const summary = screen.getByRole('heading', { name: /summary/i });
+    const about = screen.getByText(/This intelligent Pokémon/i);
+
+    expect(detailsBtn).toBeNull();
+    expect(pokemonDetails).toBeInTheDocument();
+    expect(summary).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+  });
+
+  it('Existe na página uma seção com os mapas contendo as localizações do pokémon',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push(`/pokemons/${id}`);
+      const locatinHeading = screen
+        .getByRole('heading', { name: `Game Locations of ${name}` });
+      const imgLocation = screen.getAllByAltText(`${name} location`);
+      const textLocationOne = screen.getByText('Kanto Viridian Forest');
+      const textLocationTwo = screen.getByText('Kanto Power Plant');
+
+      expect(locatinHeading).toBeInTheDocument();
+      expect(imgLocation[0]).toHaveAttribute('src', foundAt[0].map);
+      expect(textLocationOne).toBeInTheDocument();
+      expect(imgLocation[1]).toHaveAttribute('src', foundAt[1].map);
+      expect(textLocationTwo).toBeInTheDocument();
+    });
+
+  it('O usuário pode favoritar um pokémon através da página de detalhes.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(`/pokemons/${id}`);
+
+    const favCheckbox = screen.getByLabelText('Pokémon favoritado?');
+
+    userEvent.click(favCheckbox);
+    expect(favCheckbox).toBeChecked();
+  });
+});
